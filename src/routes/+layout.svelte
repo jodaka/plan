58ac7a9f26de80cea930cc22ee216dba96601c46
Anton Kudris <anton@kudris.com>
2026-08-31 12:57:48 +0300

    Tauri

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,10 @@ import favicon from '$lib/assets/favicon.svg';
 let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon}></svelte:head>
+<svelte:head>
+  <link rel="icon" href={favicon}>
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+</svelte:head>
 {@render children()}
 
 <style>
